64ebbee2290f8253ba1eb6a0c66b56597b9b9473
Juanxo Carrasco <juan.carrasco@externo.auna.org>
2025-12-28 12:22:38 -0500

    mocks and feast front

--- front/metro.config.js
+++ front/metro.config.js
@@ -3,10 +3,5 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  "react-native-worklets": require.resolve("react-native-worklets-core"),
-  "react-native-worklets/plugin": require.resolve("react-native-worklets-core/plugin"),
-};
-
 module.exports = withNativeWind(config, { input: "./global.css" });
+
