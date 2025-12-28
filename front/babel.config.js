35a67b8d83bc33418da78302e76115110e767dd8
Juanxo Carrasco <juan.carrasco@externo.auna.org>
2025-12-28 12:12:03 -0500

    mocks and feast front

--- front/babel.config.js
+++ front/babel.config.js
@@ -6,15 +6,6 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            "react-native-worklets": "react-native-worklets-core",
-            "react-native-worklets/plugin": "react-native-worklets-core/plugin",
-          },
-        },
-      ],
       "react-native-reanimated/plugin",
     ],
   };
