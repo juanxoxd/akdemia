dbdb2f4c48464f1b1888d24b8ce3bddbeb7830bc
Juanxo Carrasco <juan.carrasco@externo.auna.org>
2026-01-17 15:01:06 -0500

    avance

--- back/packages/shared-types/src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './app.constants';
-export * from './queue.constants';
-export * from './processing.constants';
--- back/packages/shared-types/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Shared Types - OMR System
-export * from './constants';
-export * from './enums';
-export * from './interfaces';
-export * from './dto';
-export * from './events';
